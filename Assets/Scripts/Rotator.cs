5ec210698ea9fa74c8ef25e759c05d534b4278d8
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-09-13 13:00:16 -0300

    Changed coin rotation

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 
-
+    public int Rotation1;
+    public int Rotation2;
+    public int Rotation3;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(Rotation1, Rotation2, Rotation3) * Time.deltaTime);
     }
 }
