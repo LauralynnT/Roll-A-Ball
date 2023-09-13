ccf2aa7101aa304cb2acc8c9793f6e77309632e2
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-09-13 10:11:48 -0300

    Added Pickup cubes
    Added cubes the player can pick up and add to the counter in the corner of the UI.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    private int count;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -23,9 +28,22 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 }
