37d04a8701f511388f1f492f811290cc045af775
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-10-16 15:32:17 -0300

    Fixed maze clipping

---
+++ Assets/Scripts/ControlsButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsButton : MonoBehaviour
+{
+    public Button Controls;
+    public GameObject ControlsParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(OpenControls);
+
+    }
+
+    // Update is called once per frame
+    void OpenControls()
+    {
+        if (ControlsParent.active == true)
+        {
+            ControlsParent.SetActive(false);   
+        }
+        else
+        {
+            ControlsParent.SetActive(true);
+        }
+    }
+}
