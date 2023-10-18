70629581986913248dffe713bf86d99717efa31c
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-10-18 15:24:26 -0300

    probably screwed everything up

---
+++ Assets/Scripts/MenuPickups.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class MenuPickups : MonoBehaviour
+{
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Debug.Log("pickup");
+            StartCoroutine(ResetPickup(other.gameObject));
+            other.gameObject.SetActive(true);
+            
+        }
+        IEnumerator ResetPickup(GameObject other)
+        {
+            other.gameObject.SetActive(false);
+            yield return new WaitForSecondsRealtime(2);
+        }
+    }
+    
+}
